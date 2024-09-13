3a477fb6cede4480d413cf6c37f2accde09639a5
Rodolfo Castro <soyracm@gmail.com>
2024-09-13 08:58:09 -0400

    Inicializando proyecto Astro para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,9 @@
 import { defineConfig } from 'astro/config';
+import github from '@astrojs/github';
+
+// https://astro.build/config
+export default defineConfig({
+  // Asegúrate de establecer el `base` si tu proyecto está en un subdirectorio en GitHub Pages
+  base: '/minimalist-portfolio-json/', // reemplaza <nombre-del-repositorio> con el nombre de tu repositorio
+  integrations: [github()]
+});
