e62527f0096f613ac337ae97dc21034b57f7af29
Alejandro Castro <150183777+r-alejandro-castro@users.noreply.github.com>
2024-09-14 20:56:04 -0400

    Desde cero

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,2 @@
-/// <reference path="../.astro/types.d.ts" />
+/// <reference path="../../../../Downloads/minimalist-portfolio-json-bcf5841e882065c8f2c2c1b45e38c346e559cdb0/.astro/types.d.ts" />
 /// <reference types="astro/client" />
